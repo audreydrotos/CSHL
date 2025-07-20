e779b35835a8a08d9e3ce57911b9fd1d9c67c9f0
huanggz1005 <112940418@qq.com>
2025-07-19 22:44:08 -0400

    pca

--- project_code/main.m
+++ project_code/main.m
@@ -108,8 +108,8 @@ end
 save(['postprocessed_data/' sesPath '_binnedTensor.mat'], 'binnedTensor')
 
 %% Plot tuning curves according to stimulus and firing rate
-%behavior = trials.contrast;%can change it
-behavior = trials.turn;
+behavior = trials.contrast;%can change it
+%behavior = trials.turn;
 behavior_value = unique(behavior);
 valueNum = length(behavior_value);
 
@@ -346,9 +346,9 @@ saveas(gcf, ['figure/' sesPath '_pca.fig']);
 %% NMF
 nFactors = 5;
 figure;
-t = tiledlayout(1, 2); 
+t = tiledlayout(1, length(regionSelected)); 
 
-for rr = 1:2
+for rr = 1:length(regionSelected)
     region_code = regionSelected(rr);
     region_idx = neurons.region == region_code;
     region_neurons = tensorPCA(region_idx, :, :);
@@ -377,29 +377,60 @@ saveas(gcf, ['figure/' sesPath '_nmf.fig']);
 %% Umap
 % Use 'run_umap' to reduce the dim to 3
 % try different values for n_neighbors ranging from 5 to 199
-n_components = 3;
+% Umap need to classify neurons
+n_components = 2;
 n_neighbors = 10;
 figure;
-t = tiledlayout(1, 2); 
+t = tiledlayout(1, length(regionSelected)); 
 
-for rr = 1:2
+for rr = 1:length(regionSelected)
     region_code = regionSelected(rr);
     region_idx = neurons.region == region_code;
-    region_neurons = tensorPCA(region_idx, :, :);
+    
      % Generate UMAP
     nexttile(rr)
+    region_neurons = binnedTensor(region_idx, :, :);
+
+    summedTensor = sum(region_neurons,2);
+    neuronNum = size(region_neurons,1);
+    
+    firing_Onbehavior = zeros(valueNum,neuronNum);
+    time_Onbehavior = zeros(valueNum,neuronNum);
+
+    for i = 1:neuronNum
+        for j = 1:nTrials
+            for v = 1:valueNum
+                if behavior(j)==behavior_value(v)
+                    firing_Onbehavior(v,i) = firing_Onbehavior(v,i)+summedTensor(i, 1, j);
+                    time_Onbehavior(v,i) = time_Onbehavior(v,i)+1;
+                end
+            end
+        end
+    end
+
+    firingRate_Onbehavior = firing_Onbehavior./time_Onbehavior;
+    [peak_values, peak_stimuli] = max(firingRate_Onbehavior, [], 1);
 
     % find
+    region_neurons = tensorPCA(region_idx, :, :);
     averageTrials = mean(region_neurons,3);
-    [rep_UMAP, umap, clusterIdentifiers, extras]=run_umap(double(averageTrials'), ...
+    rng(42);
+    [rep_UMAP, umap, clusterIdentifiers, extras]=run_umap(double(averageTrials), ...
     'n_components', n_components, 'n_neighbors', n_neighbors, 'verbose', 'none');
     % plot coef by signals
-    plot3(rep_UMAP(:,1), rep_UMAP(:,2), rep_UMAP(:,3),colorSelected(rr));
+    % plot using different neurons
+    color_sequence = parula(valueNum); 
+
+    for i = 1:valueNum
+        currentUmap= rep_UMAP(peak_stimuli==i,:);
+        scatter(currentUmap(:,1), currentUmap(:,2),'filled');
+        hold on
+    end
+    
     xlabel('UMAP 1')
     ylabel('UMAP 2')
-    zlabel('UMAP 3')
+    %zlabel('UMAP 3')
     title('UMAP on',regions.name(region_code))
-    hold on
 
 end
 
